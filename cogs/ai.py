6e287d273b0dd2153d1176bf14cf67fe7f10a97f
BelloLeSlime <jm.soum@outlook.fr>
2026-08-21 15:51:29 +0200

    Add /shop_view and add toggleable features

--- cogs/ai.py
+++ cogs/ai.py
@@ -21,6 +21,14 @@ class Ai(commands.Cog):
         :param prompt: Demande faite à l'IA
         :return:
         """
+
+        config = Cf.get_config(ctx.guild.id)
+        if not config["enable_ai"]:
+            await ctx.send(embed=discord.Embed(color=discord.Color.red(),
+                                               description=f"Désolé, mais l'IA n'est pas activé sur ce serveur !"),
+                           ephemeral=True)
+            return
+
         if ctx.guild:
             await ctx.defer()
 
@@ -64,7 +72,6 @@ class Ai(commands.Cog):
             Cf.write_file(author + " : " + content, f"files/dms/{ctx.author.id}.txt")
             Cf.write_file("BelloBot(forbellobot) : " + to_send, f"files/dms/{ctx.author.id}.txt")
 
-
     @commands.hybrid_command(name="vote_reset_memory")
     async def vote_reset_memory(self, ctx: commands.Context):
         """
@@ -72,6 +79,13 @@ class Ai(commands.Cog):
         :param ctx: Context
         :return:
         """
+
+        config = Cf.get_config(ctx.guild.id)
+        if not config["enable_ai"]:
+            await ctx.send(embed=discord.Embed(color=discord.Color.red(),
+                                               description=f"Désolé, mais l'IA n'est pas activé sur ce serveur !"), ephemeral=True)
+            return
+
         await ctx.send("Vote organisé !", ephemeral=True)
         embed = discord.Embed(color= discord.Color.orange(), title="Voulez-vous réinitialiser ma mémoire car je deviens fou ?", description="Réagir 👍 pour oui.").set_footer(text="Fin du vote dans 1min !")
         msg = await ctx.channel.send(embed=embed)
@@ -106,55 +120,6 @@ class Ai(commands.Cog):
         else:
             await ctx.channel.send("Ma mémoire ne sera pas réinitialisée !")
 
-    @commands.hybrid_command(name="generate_image")
-    async def generate_image(self, ctx: commands.Context, prompt: str, negative_prompt: str = "", width: int = 1024, height: int = 1024, steps: int = 30):
-        """
-        Génère une image pour la modique somme de 500₣ + le nombre d'étapes
-        :param ctx: 
-        :param prompt: Description de l'image
-        :param negative_prompt: Ce qu'il n'y a pas dans l'image
-        :param width: Largeur de l'image en pixels
-        :param height: Hauteur de l'image en pixels
-        :param steps: Étapes (donc qualité grossomodo)
-        :return: 
-        """
-
-        #Commande désactivée à cause du prix des IA, commentez ou supprimez si vous avec les moyens
-        embed = discord.Embed(color= discord.Color.red(), description="Commande temporariement désactivée. Désolé !")
-        return
-
-        await ctx.defer()
-
-        user_data = Cf.read_json(f"files/user_info/{ctx.guild.id}/{ctx.author.id}.json")
-        if user_data["money"] >= 500 + steps:
-
-            nude_str: str = Cf.ask_ai([{"role": "system",
-                                        "content": "Tu dois déterminer si le prompt suivant pour générer une image est adéquat. Ex: pas de nude, d'image sexualisée, de gore, ou de contenu pouvant choquer. Tu répondra qu'avec 'Y' ou 'N'. Y pour ça passe et N pour empêcher l'utilisateur"},
-                                       {"role": "user", "content": prompt}], model)
-
-            if nude_str.__contains__("Y"):
-                nude = False
-            elif nude_str.__contains__("N"):
-                nude = True
-            else:
-                nude = None
-
-            if not nude is None:
-                if nude:
-                    await ctx.send(
-                        f"Regardez, {ctx.author.mention} a essayé de générer une image de {prompt} mais a pas réussi ce nul XD \n Allez 1 jour de mute pour toi :p")
-                    await ctx.author.timeout(timedelta(days=1), reason="Essaie de générer une image suspecte")
-                else:
-                    user_data["money"] -= 500
-                    Cf.write_json(user_data, f"files/user_info/{ctx.guild.id}/{ctx.author.id}.json")
-                    image = Cf.text_to_image(prompt, image_model, negative_prompt, width, height, steps)
-                    await Cf.send_image(ctx, image)
-            else:
-                await ctx.send("AAaah j'arrive pas à décider si ça passe ou non jsp quoi faire")
-        else:
-            await ctx.send(
-                f"Tu n'a pas assez d'argent pour générer une image ! La génération d'image coûte 500₣ + le nombre d'étapes (ici {steps}) pour éviter le spam et la déchéance économique de Bello le Slime.")
-
     @commands.hybrid_command(name="reset_dm_memory")
     async def reset_dm_memory(self, ctx: commands.Context):
         """
@@ -163,13 +128,15 @@ class Ai(commands.Cog):
         :return:
         """
 
+        if ctx.guild:
+            await ctx.send("Cette commande n'est utilisable que dans des messages privées.", ephemeral=True)
+
         user_id = ctx.author.id
         with open(f"files/dms/{user_id}.txt", "w", encoding="utf-8") as f:
             f.write("")
 
         embed = discord.Embed(color=discord.Color.orange(), description="Ma mémoire dans les MP est réinitialisée.")
         await ctx.send(embed=embed, ephemeral=True)
-
 
 async def setup(bot):
     await bot.add_cog(Ai(bot))
